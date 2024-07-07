47c0bdfb830fde6127c3d01588a9ddf326a3eede
Peter Cai <petercaica@hotmail.com>
2024-07-07 19:25:02 -0400

    LoginManager

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,8 @@ from flask_login import LoginManager
 from flask_mongoengine import MongoEngine
 from flask_pymongo import PyMongo
 
+from app.config import DebugConfig
+
 """
  Logging configuration
 """
@@ -17,27 +19,28 @@ logging.getLogger().setLevel(logging.DEBUG)
 
 db = MongoEngine()
 appbuilder = AppBuilder(security_manager_class=SecurityManager)
-# login_manager = LoginManager()
+login_manager = LoginManager()
 
 
 def register_extensions(app):
     db.init_app(app)
     appbuilder.init_app(app, session=None)
-    # login_manager.init_app(app)
+    login_manager.init_app(app)
 
 
 
 def register_blueprints(app):
-    # for module_name in ('authentication', 'home'):
-    for module_name in ['home']:
+    for module_name in ('authentication', 'home'):
         module = import_module('app.{}.routes'.format(module_name))
         app.register_blueprint(module.blueprint)
     pass
 
 
 def create_app(config):
+# def create_app():
     app = Flask(__name__)
     app.config.from_object(config)
+    # app.config.from_object(DebugConfig())
     register_extensions(app)
     register_blueprints(app)
 
